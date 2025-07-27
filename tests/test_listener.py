4448d01cc82cd17187e452576fc5533205868db2
CSP <16367956+CSP197@users.noreply.github.com>
2025-07-26 18:18:21 -0700

    Add tests for QRCodeListener start/stop cycles

--- tests/test_listener.py
+++ tests/test_listener.py
@@ -1,7 +1,55 @@
 from openqr.app import QRCodeListener
 import pytest
 
-listener = QRCodeListener()
+@pytest.fixture
+def listener():
+    return QRCodeListener()
 
-def test_initial_state():
+def test_initial_state(listener):
+    assert len(listener.buffer) == 0
     assert listener.is_listening == False
+
+def test_final_state(listener):
+    assert listener.is_listening == False
+
+def test_start_listening(listener):
+    listener.start_listening()
+
+    assert listener.is_listening == True
+
+def test_stop_listening(listener):
+    listener.stop_listening()
+
+    assert listener.is_listening == False
+
+def test_single_full_cycle(listener):
+    assert len(listener.buffer) == 0
+    assert listener.is_listening == False
+
+    listener.start_listening()
+    assert listener.is_listening == True
+
+    listener.stop_listening()
+    assert listener.is_listening == False
+
+def test_multiple_full_cycles(listener):
+    assert len(listener.buffer) == 0
+    assert listener.is_listening == False
+
+    listener.start_listening()
+    assert listener.is_listening == True
+
+    listener.stop_listening()
+    assert listener.is_listening == False
+
+    listener.start_listening()
+    assert listener.is_listening == True
+
+    listener.stop_listening()
+    assert listener.is_listening == False
+
+def test_start_when_already_started(listener):
+    listener.start_listening()
+    assert listener.is_listening == True
+    listener.start_listening()
+    assert listener.is_listening == True
