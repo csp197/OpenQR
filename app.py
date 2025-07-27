a0c416c2c12d52f7f4577aeddd8ee0df294d8f6f
CSP <16367956+CSP197@users.noreply.github.com>
2025-07-26 17:55:46 -0700

    Add basic OpenQR app UI and listener toggle functionality

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import sys
 
-from PyQt5.QtCore import QObject, QSize, Qt, pyqtSignal
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtCore import QObject, QSize, QState, Qt, pyqtSignal
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QStatusBar, QVBoxLayout, QWidget
+
+from .constants import START_LISTENING_MSG, STOP_LISTENING_MSG, INACTIVE_LISTENER_MSG, ACTIVE_LISTENER_MSG
 
 class QRCodeListener(QObject):
     url_opened = pyqtSignal(str, str) # title, message
@@ -9,15 +11,54 @@ class QRCodeListener(QObject):
     def __init__(self, timeout=1.0, allowed_domains=None):
         super().__init__()
         self.buffer = ""
+        self.is_listening = False
+        return
+
+    def start_listening(self):
+        self.is_listening = True
+        return
+
+    def stop_listening(self):
+        self.is_listening = False
+        return
 
 
 class OpenQRApp(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("OpenQR - QR Code URL Listener")
-        self.setMinimumSize(400, 400)
+        self.setWindowTitle("OpenQR")
+        self.setMinimumSize(800, 800)
 
-        self.listener = QRCodeListener(timeout=1.0)
+        # instantiate QR code listener
+        self.qr_code_listener = QRCodeListener(timeout=1.0)
+
+        # creating main widget and layout
+        main_widget = QWidget()
+        layout = QVBoxLayout()
+        # setting layout and central widget
+        main_widget.setLayout(layout)
+        self.setCentralWidget(main_widget)
+
+        # creating toggle button
+        self.toggle_btn = QPushButton(START_LISTENING_MSG)
+        self.toggle_btn.pressed.connect(self.toggle_listener)
+        layout.addWidget(self.toggle_btn)
+
+        # creating status bar
+        self.status_bar = QStatusBar()
+        self.setStatusBar(self.status_bar)
+        self.status_bar.showMessage(INACTIVE_LISTENER_MSG)
+
+    def toggle_listener(self):
+        if self.qr_code_listener.is_listening:
+            self.qr_code_listener.stop_listening()
+            self.toggle_btn.setText(STOP_LISTENING_MSG)
+            self.status_bar.showMessage(ACTIVE_LISTENER_MSG)
+        else:
+            self.qr_code_listener.start_listening()
+            self.toggle_btn.setText(START_LISTENING_MSG)
+            self.status_bar.showMessage(INACTIVE_LISTENER_MSG)
+
 
 
 if __name__ == "__main__":
