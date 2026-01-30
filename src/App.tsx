75164fdbf1c09ef6255f1c4ad0fb69177c4282d9
CSP <16367956+CSP197@users.noreply.github.com>
2026-01-29 17:35:02 -0800

    Adding to main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
+
 import { listen } from "@tauri-apps/api/event";
 import { invoke } from "@tauri-apps/api/core";
+
 import Header from "./components/header/header";
 import Reader from "./components/url_reader/reader";
 import Generator from "./components/qr_code_generator/generator";
